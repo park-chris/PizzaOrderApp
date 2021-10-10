2e7085531ff2f00da6687569802badcdc46179c7
박수정 <zxzx4342@naver.com>
2021-10-10 14:34:06 +0900

    확인버튼 누르면 -> 닉네임 + OK 신호 담아서 복귀

--- app/src/main/java/com/neppplus/pizzaorderapp_20211010/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211010/EditNicknameActivity.kt
@@ -1,11 +1,26 @@
 package com.neppplus.pizzaorderapp_20211010
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {
+
+            val inputNickname = nicknameEdt.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+
+
+        }
+
     }
 }
