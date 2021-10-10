c12b1f7aa5325c095fc385fdb8fb687c87a61a5e
박수정 <zxzx4342@naver.com>
2021-10-10 14:39:50 +0900

    프래그먼트로 돌아와서 -> 새 닉네임 반영

--- app/src/main/java/com/neppplus/pizzaorderapp_20211010/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211010/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.pizzaorderapp_20211010.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -30,9 +31,21 @@ class MyProfileFragment : Fragment() {
             val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
             startActivityForResult(myIntent, REQ_FOR_NICKNAME )
 
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_FOR_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
+
+//                data는 Intent이기에 혹시 값이 null일수도 있으니 아래처럼 처리
+                val newNick = data!!.getStringExtra("nickname")
+                nicknameTxt.text = newNick
+            }
         }
 
-
     }
+
 }
