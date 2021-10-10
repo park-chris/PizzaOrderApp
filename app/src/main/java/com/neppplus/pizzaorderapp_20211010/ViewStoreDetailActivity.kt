8bd7faa5c4fd70acfda635bd3338c0c072eb6b43
박수정 <zxzx4342@naver.com>
2021-10-10 14:02:56 +0900

    전화걸기 누르면 -> 전화 (CALL) 권한 확인  - TedPermission으로 권한 체크  - 우선 거절 상황만 코딩

--- app/src/main/java/com/neppplus/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
@@ -1,13 +1,61 @@
 package com.neppplus.pizzaorderapp_20211010
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import com.neppplus.pizzaorderapp_20211010.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import kotlinx.android.synthetic.main.pizza_store_list_item.*
+import kotlinx.android.synthetic.main.pizza_store_list_item.logoImg
+import kotlinx.android.synthetic.main.activity_view_store_detail.storeNameTxt as storeNameTxt1
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
 
+
+//        시리얼라이저블 형태를 StoreData 형식으로 변환
+        val storeData = intent.getSerializableExtra("store") as StoreData
+
+        Glide.with(this).load(storeData.logoURL).into(logoImg)
+
+        storeNameTxt.text = storeData.name
+        phoneNumTxt.text = storeData.phoneNum
+
+
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(
+                        this@ViewStoreDetailActivity,
+                        "통화 권한이 거절되었습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+
+                }
+
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
 
     }
--- app/src/main/java/com/neppplus/pizzaorderapp_20211010/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211010/fragments/PizzaStoreListFragment.kt
@@ -41,7 +41,7 @@ class PizzaStoreListFragment : Fragment() {
 
             val myIntent = Intent(requireContext(), ViewStoreDetailActivity::class.java)
             myIntent.putExtra("store", clickedStore)
-
+            startActivity(myIntent)
         }
 
     }
